9944acfcae6111f59de1680d3395d442fbc4ad55
Tatjana Raznatovic <traznatovic@novomaticls.com>
2017-04-11 13:06:44 +0200

    first commit

---
+++ src/main/java/com/mongodb/HelloWorldSparkStyle.java
@@ -0,0 +1,37 @@
+package com.mongodb;
+
+
+import spark.Route;
+import spark.Spark;
+import spark.Request;
+import spark.Response;
+
+
+/**
+ * todo: class comment
+ *
+ * @author traznatovic
+ */
+public class HelloWorldSparkStyle
+{
+  public static void main(String[] args)
+  {
+    Spark.get ("/",new Route()
+    {
+      @Override
+      public Object handle(final Request request, final Response response)
+      {
+        return "Hello again from Spark";
+      }
+    });
+    Spark.get ("/hello",new Route()
+        {
+          @Override
+          public Object handle(final Request request, final Response response)
+          {
+            return "A sad nešto sasvim drugačije!";
+          }
+        });
+
+  }
+}
